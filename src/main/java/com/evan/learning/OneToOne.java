92c2810c625e0ebfb623988177e885c8014d9202
evxndakara <evanbingotz@gmail.com>
2021-03-14 10:33:04 +0700

    initial commit

---
+++ src/main/java/com/evan/learning/OneToOne.java
@@ -0,0 +1,33 @@
+package com.evan.learning;
+
+import com.evan.learning.hibernate.model.onetoone.Identity;
+import com.evan.learning.hibernate.model.onetoone.Person;
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+import java.util.List;
+
+public class OneToOne {
+    public static void main(String[] args) {
+        // create session factory object
+        SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+
+        // create session object
+        Session session = sessionFactory.openSession();
+
+        // create transaction object
+        Transaction transaction = session.beginTransaction();
+
+        List<Person> personList = session.createQuery("from Person", Person.class).list();
+
+        for (Person person : personList) {
+            System.out.println(person.getName());
+        }
+
+        transaction.commit();
+        session.close();
+    }
+}
